36a0bad1b12216667a89fb496d40c66cdc355981
Banskiie <ivansinohon28@gmail.com>
2023-06-07 09:57:33 +0800

    removed reflection link

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Profile from "../pages/Profile";
 import Journal from "../pages/Journal";
-import Reflection from "../pages/Reflection";
 
 const AppRoutes = () => {
   return (
@@ -11,7 +10,6 @@ const AppRoutes = () => {
       <Route index path="/" Component={Home} />
       <Route path="/profile" Component={Profile} />
       <Route path="/journal" Component={Journal} />
-      <Route path="/reflection" Component={Reflection} />
     </Routes>
   );
 };
